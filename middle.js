60d3f31c2642cea4104ebaf3eb10309057fa8f23
Moises Vivas <moisesvivass@gmail.com>
2025-07-31 20:53:37 -0400

    Middle

---
+++ middle.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const middle = function(array) {
+  const length = array.length;
+
+  if (length <= 2) {
+    return [];
+  }
+
+  const mid = Math.floor(length / 2);
+
+  if (length % 2 === 0) {
+    return [array[mid - 1], array[mid]];
+  } else {
+    return [array[mid]];
+  }
+};
